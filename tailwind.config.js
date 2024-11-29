eecb0c888e79a75dab293fa4b6d44b5b819f7a35
pgraverdy <pgraverdy@gmail.com>
2024-11-29 12:04:44 +0100

    Cleanup of color palette use (removing hardcoded values)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,53 +1,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+const colors = require('tailwindcss/colors')
+
 // tailwind.config.js
 module.exports = {
   content: ['./layouts/**/*.html', './content/**/*.md'],
   darkMode: 'class',
   theme: {
+    colors: {
+      primary: colors.orange,
+      secondary: colors.lime,
+      neutral: colors.gray,
+    },
     extend: {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        'primary': {
-          DEFAULT: '#6366F1',
-          50: '#FFFFFF',
-          100: '#F9F9FE',
-          200: '#D3D4FB',
-          300: '#AEAFF8',
-          400: '#888BF4',
-          500: '#6366F1',
-          600: '#3034EC',
-          700: '#1317D1',
-          800: '#0E119E',
-          900: '#0A0C6A'
-        },
-        'secondary': {
-          DEFAULT: '#EC4899',
-          50: '#FDEEF6',
-          100: '#FBDCEB',
-          200: '#F8B7D7',
-          300: '#F492C2',
-          400: '#F06DAE',
-          500: '#EC4899',
-          600: '#E4187D',
-          700: '#B11261',
-          800: '#7F0D45',
-          900: '#4C0829'
-        },
-        'neutral': {
-          DEFAULT: '#6B7280',
-          50: '#CDD0D5',
-          100: '#C2C5CC',
-          200: '#ACB0BA',
-          300: '#969BA7',
-          400: '#7F8694',
-          500: '#6B7280',
-          600: '#515761',
-          700: '#383C43',
-          800: '#1E2024',
-          900: '#050506'
-        },
         // To change these, use https://www.tailwindshades.com/ with https://tailwindcss.com/docs/customizing-colors or create your own custom colors.
       },
       lineHeight: {
@@ -68,15 +36,15 @@ module.exports = {
             '--tw-prose-quotes': theme('colors.zinc[900]'),
             '--tw-prose-quote-borders': theme('colors.zinc[300]'),
             '--tw-prose-captions': theme('colors.zinc[700]'),
-            '--tw-prose-code': theme('colors.indigo[500]'),
-            '--tw-prose-pre-code': theme('colors.indigo[300]'),
+            '--tw-prose-code': theme('colors.primary[500]'),
+            '--tw-prose-pre-code': theme('colors.primary[300]'),
             '--tw-prose-pre-bg': theme('colors.gray[900]'),
             '--tw-prose-th-borders': theme('colors.zinc[300]'),
             '--tw-prose-td-borders': theme('colors.zinc[200]'),
             '--tw-prose-invert-body': theme('colors.zinc[200]'),
             '--tw-prose-invert-headings': theme('colors.white'),
             '--tw-prose-invert-lead': theme('colors.zinc[300]'),
-            '--tw-prose-invert-links': theme('colors.indigo[400]'),
+            '--tw-prose-invert-links': theme('colors.primary[400]'),
             '--tw-prose-invert-bold': theme('colors.white'),
             '--tw-prose-invert-counters': theme('colors.zinc[400]'),
             '--tw-prose-invert-bullets': theme('colors.zinc[200]'),
@@ -84,8 +52,8 @@ module.exports = {
             '--tw-prose-invert-quotes': theme('colors.zinc[100]'),
             '--tw-prose-invert-quote-borders': theme('colors.zinc[700]'),
             '--tw-prose-invert-captions': theme('colors.zinc[400]'),
-            '--tw-prose-invert-code': theme('colors.indigo[400]'),
-            '--tw-prose-invert-pre-code': theme('colors.indigo[300]'),
+            '--tw-prose-invert-code': theme('colors.primary[400]'),
+            '--tw-prose-invert-pre-code': theme('colors.primary[300]'),
             '--tw-prose-invert-pre-bg': theme('colors.gray[900]'),
             '--tw-prose-invert-th-borders': theme('colors.zinc[100]'),
             '--tw-prose-invert-td-borders': theme('colors.zinc[500]'),
